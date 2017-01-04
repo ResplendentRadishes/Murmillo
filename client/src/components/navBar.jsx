53e09d6433a175ef84a0010f0a3b5d28aec7efeb
Vernon Quan <vernonquan@gmail.com>
2017-01-04 15:22:34 -0800

    (feat) Connected Sockets

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -8,6 +8,9 @@ const NavBar = (props) => (
       <Link to="/">
         <h3 className="navbar-text">Murmillo</h3>
       </Link>
+      <Link to="/arena">
+        <h5 className="navbar-text">Super Secret Dev Link</h5>
+      </Link>
     </div>
     <button type="button" className="btn btn-default navbar-btn navbar-right">Sign in</button>
   </div>
--- client/src/components/roomEntry.jsx
+++ client/src/components/roomEntry.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { joinRoom } from '../socketHandler.js';
 
 const RoomEntry = (props) => (
   <div>
@@ -12,9 +13,12 @@ const RoomEntry = (props) => (
           <p>{props.room.desc}</p>
         </div>
         <div className="col-md-4 text-right no-gutter">
-          <Link to="/arena">
-            <button type="button" className="btn btn-success btn-primary btn-lg">Join Room</button>
-          </Link>
+          <button 
+            type="button" 
+            className="btn btn-success btn-primary btn-lg" 
+            onClick={() => joinRoom(props.room.name)}
+          >Join Room
+          </button>
         </div>
       </div>
     </div>
