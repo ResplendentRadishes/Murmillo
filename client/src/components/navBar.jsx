f1d657075309e1fb4ddfbab046e8e96e25b1bdd6
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-01-09 12:36:31 -0800

    (feat/home) changes navbar redirect
    app name "Murmillo" in navbar now redirects to '/dashboard'

--- client/src/components/navBar.jsx
+++ client/src/components/navBar.jsx
@@ -5,15 +5,15 @@ const NavBar = (props) => (
   <nav className="navbar navbar-default">
   <div className="container-fluid">
     <div className="navbar-header">
-      <Link to="/">
+      <Link to="/dashboard">
         <h3 className="navbar-text">Murmillo</h3>
       </Link>
     </div>
     <div className ="navbar-right">
     {props.user.username ?
       <div>
-        <p className="navbar-text">{'Logged in as ' + props.user.username}</p> 
-        <button className="btn btn-default navbar-btn" onClick={() => props.signOut()}>Sign out</button> 
+        <p className="navbar-text">{'Logged in as ' + props.user.username}</p>
+        <button className="btn btn-default navbar-btn" onClick={() => props.signOut()}>Sign out</button>
       </div>
       :
       <Link to='/signup' className="btn btn-default navbar-btn">Sign in</Link>
