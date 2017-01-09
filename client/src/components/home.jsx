b4055230d03c3f5a8b4b6cfb23e1a0e18461c00e
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-01-09 12:15:24 -0800

    (feat/homepage) makes home more centered

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+const flexboxStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '70vh'
+}
+
 const Home = (props) => (
-  <div className='text-center'>
-    <img src='./spqr-murmillo-2.jpg'/>
-    <h1 style={{'font-size': '72px'}}>Murmillo</h1>
-    <Link to='/signup' className='btn btn-info btn-lg'>Start</Link>
+  <div style={flexboxStyle}>
+    <div className='text-center'>
+      <img src='./spqr-murmillo-2.jpg'/>
+      <h1 style={{fontSize: '72px'}}>Murmillo</h1>
+      <Link to='/signup' className='btn btn-info btn-lg'>Start</Link>
+    </div>
   </div>
 );
 
