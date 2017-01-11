9526062e1067755802e774ef62faacf881739d45
Yoshi Oritatsu <yoshioritatsu@gmail.com>
2017-01-11 15:38:51 -0800

    (refactor/socket) - configure all serverSocket listeners when user clicks 'join' (to avoid setting same listeners multiple times)

--- client/src/pageDashboard/roomContainer.jsx
+++ client/src/pageDashboard/roomContainer.jsx
@@ -1,5 +1,12 @@
 import { connect } from 'react-redux';
-import { setRoom, updateMessages } from '../actions/actions.js';
+import  {
+          setRoom,
+          updateMessages,     // used with socketOnMsg
+          setProblem,         // used with socketOnProblem
+          receiveCodeCheck,   // used with socketOnSubmission
+          getCompUpdate       // used with socketOnUpdate
+        } from '../actions/actions.js';
+
 import RoomEntry from './roomEntry.jsx';
 
 const mapStateToProps = (state, ownProps) => {
@@ -12,12 +19,21 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    updateMessages: function(message) {
-      dispatch(updateMessages(message));
-    },
-    updateCurrentRoom: function(room) {
+    setRoom: function(room) {
       dispatch(setRoom(room));
-    }
+    },
+    updateMessages: function(serverMessage) {
+      dispatch(updateMessages(serverMessage));
+    },
+    setProblem: function(problem) {
+      dispatch(setProblem(problem));
+    },
+    receiveCodeCheck: function(result) {
+      dispatch(receiveCodeCheck(result));
+    },
+    getCompUpdate: function(update) {
+      dispatch(getCompUpdate(update));
+    },
   }
 }
 
