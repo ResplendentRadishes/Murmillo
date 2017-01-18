20b971e9eee9e8ab763463e2b4f003ce9a205c1e
Robert Littlejohn <bbblittlejohn@gmail.com>
2017-01-17 17:40:14 -0800

    added back fakeData
    since we still have not fixed this app to get the actual data

---
+++ fakeData/fakeCompList.js
@@ -0,0 +1,27 @@
+// =========================================
+// fakeCompList
+var fakeCompList = [{
+  id: 1,
+  problemId: 3,
+  name: 'hard',
+  desc: 'This is tough yo',
+  messages: []
+},
+{
+  id: 2,
+  problemId: 2,
+  name: 'medium',
+  desc: 'Not that bad',
+  messages: []
+},
+{
+  id: 3,
+  problemId: 1,
+  name: 'easy',
+  desc: 'Really?',
+  messages: []
+}];
+
+// =========================================
+
+module.exports = fakeCompList;
